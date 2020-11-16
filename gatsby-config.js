bdf07c36ae0315bbd3886de2393948efc67eebdd
BielDiaz <biel.diaz@gmail.com>
2020-11-16 16:22:41 +0100

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `TRAVEL PAGE`,
+    description: `Travel website showcasing the best travel destinations and deals online.`,
+    author: `@bieldg99`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-styled-components',
+
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,26 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import styled from 'styled-components'
+import { Link } from "gatsby"
+const Header = () => {
+  return (
+    <Nav>
+      <NavLink to="/">EXPLORIX</NavLink>
+    </Nav>
+  )
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
+const Nav = styled.nav`
+  background:red;
+  height:80px;
+  display:flex;
+  justify-content:space-between;
+  padding: 0.5rem;
+  
+`
+
+const NavLink = styled(Link)`
+
+`
