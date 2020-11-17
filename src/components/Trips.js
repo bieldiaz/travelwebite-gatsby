1fa85244b112ffd877becd5d681e9ca1e5bbaabe
BielDiaz <biel.diaz@gmail.com>
2020-11-17 17:25:21 +0100

    Implemented featured trips section

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -2,8 +2,11 @@ import React from 'react'
 import styled from 'styled-components'
 import { useStaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
+import { Button } from './Button'
+import { ImLocation } from 'react-icons/im'
 
-const Trips = () => {
+
+const Trips = ({ heading }) => {
     const dataQuery = useStaticQuery(graphql`
         query TripsQuery {
             allTripsJson {
@@ -29,10 +32,23 @@ const Trips = () => {
         const tripsArray = []
         dataQuery.allTripsJson.edges.forEach((item, index) => {
             tripsArray.push(
-                <div key={index}>
-                    <Img src={item.node.img.childImageSharp.fluid.src}
-                        fluid={item.node.img.childImageSharp.fluid} />
-                </div>
+                <ProductCard key={index}>
+                    <ProductImg src={item.node.img.childImageSharp.fluid.src}
+                        fluid={item.node.img.childImageSharp.fluid}
+                        alt={item.node.alt} />
+                    <ProductInfo>
+                        <TextWrap>
+                            <ImLocation />
+                            <ProductTitle>{item.node.name}</ProductTitle>
+                        </TextWrap>
+                        <Button to="/trips"
+                            primary="true"
+                            round="true"
+                            css={`position:absolute; top:420px;font-size:14px`}>
+                            {item.node.button}
+                        </Button>
+                    </ProductInfo>
+                </ProductCard>
             )
         })
         return tripsArray
@@ -40,8 +56,10 @@ const Trips = () => {
 
     return (
         <ProductsContainer>
-            <ProductsHeading>Heading</ProductsHeading>
-            <ProductWrapper>{getTripsJson(dataQuery)}</ProductWrapper>
+            <ProductsHeading>{heading}</ProductsHeading>
+            <ProductWrapper>
+                {getTripsJson(dataQuery)}
+            </ProductWrapper>
         </ProductsContainer>
     )
 }
@@ -51,7 +69,7 @@ export default Trips
 const ProductsContainer = styled.div`
     min-height:100vh;
     padding: 5rem calc(100vw - 1300px)/2);
-    background:red;
+    background:#fff;;
     color: #fff;
 `
 
@@ -63,5 +81,59 @@ const ProductsHeading = styled.div`
 `
 
 const ProductWrapper = styled.div`
+    display:grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 10px;
+    justify-items:center;
+    padding:0 2rem;
 
+    @media screen and (max-width:1200px){
+        grid-template-columns: 1fr 1fr;
+    }
+    @media screen and (max-width:868px){
+        grid-template-columns: 1fr;
+    }
+`
+
+const ProductCard = styled.div`
+    line-height:2;
+    width:100%;
+    height:500px;
+    position:relative;
+    border- :10px;
+    transition: 0.2s ease;
+`
+const ProductImg = styled(Img)`
+    height:100%;
+    max-width:100%;
+    position:absolute;
+    border-radius: 10px;
+    filter: brightness(70%);
+    transition: 0.4s cubic-bezier(0.075, 0.82, 0.165, 1);
+    &:hover{
+        filter: brightness(100%);
+    }
+`
+
+const ProductInfo = styled.div`
+    display:flex;
+    flex-direction:column;
+    align-items:flex-start;
+    padding:0 2rem;
+    @media screen and (max-width:280px){
+        padding: 0 1rem;
+    }
+`
+
+const TextWrap = styled.div`
+    display:flex;
+    align-items:center;
+    position:absolute;
+    top:375px;
+`
+
+const ProductTitle = styled.div`
+    font-weight:400;
+    font-size: 1rem;
+    margin-left: 0.5rem;
 `
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
-    <Trips />
+    <Trips heading="Our Favourite Destinations" />
   </Layout>
 )
 
